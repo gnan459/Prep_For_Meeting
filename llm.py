ec563655c0f7984d8b1b5b683f3fff3ed23308e7
gnan459 <145243791+gnan459@users.noreply.github.com>
2025-07-10 06:04:10 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -7,9 +7,5 @@ load_dotenv()
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=os.getenv("GEMINI_API_KEY"),
-    # temperature=0.4,
-    # convert_system_message_to_human=True,
-    # max_output_tokens=2048,
-    # request_timeout=120,
-    # streaming=False
+    
 )
